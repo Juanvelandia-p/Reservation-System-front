495df0ecf20813eb85cb751441b6e856bd6c4dfb
unknown <daniel.ruge-g@labinfo.is.escuelaing.edu.co>
2025-03-20 15:50:25 -0500

    arreglo final 6

--- src/ReservaPage.js
+++ src/ReservaPage.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios"; // Importamos axios
 import "./styles/ReservaPage.css"; // Importamos los estilos
 import { enviarReserva } from "./hooks/labhooks.tsx";
 
